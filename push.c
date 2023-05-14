090260043f02a0eaaed25e4e75fbde822b64cce2
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-05-14 00:36:33 +0000

    pint updated

--- push.c
+++ push.c
@@ -1,28 +1,36 @@
 #include "monty.h"
 
-/**
- * push - adds node to the start of dlinkedlist
- * @h: head of linked list (node at the bottom of stack)
- * @line_number: bytecode line number
- * @n: integer
+/*
+ * push - pushes an element to the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number of the command in the Monty bytecode file
  */
 
-void push(stack_t **h, unsigned int line_number, const char *n)
+void push(stack_t **stack, unsigned int line_number)
 {
-	if (!h)
-		return;
-	if (is_number(n) == -1)
+	stack_t *new_node;/*Declare a pointer to the new node*/
+
+	/*No stack or no argument, print an error message and exit*/
+	if (!stack || !global_variable->argument)
 	{
-		printf("L%u: usage: push integer\n", line_number);
-		free_dlist(h);
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	else
+
+	/*Allocate memory for the new node and check for allocation failure*/
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
 	{
-		if (add_end_node(h, atoi(n)) == -1)
-		{
-			free_dlist(h);
-			exit(EXIT_FAILURE);
-		}
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
+
+	/*Assign the integer value from the argument to the new node*/
+	new_node->n = atoi(global_variable->argument);
+	new_node->prev = NULL;/*Set the 'prev' pointer of the new node to NULL*/
+	/*Set the next pointer of the new node to the curent head of the stack*/
+	new_node->next = *stack;
+	if (*stack)/*If there's an existing head of the stack*/
+		(*stack)->prev = new_node;/*Set its 'prev' pointer to the new node*/
+	*stack = new_node;/*Set the head of the stack to the new node*/
 }
